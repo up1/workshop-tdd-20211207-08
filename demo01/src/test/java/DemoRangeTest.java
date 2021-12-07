7735abcdf5fe65a4275b0aefb493bb2097f870d8
up1 <somkiat.p@gmail.com>
2021-12-07 10:37:02 +0700

    Second case

--- demo01/src/test/java/DemoRangeTest.java
+++ demo01/src/test/java/DemoRangeTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DemoRangeTest {
@@ -11,6 +12,14 @@ public class DemoRangeTest {
         DemoRange demoRange = new DemoRange("[1,5]");
         boolean result = demoRange.startWithInclude();
         assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("ต้องเริ่มต้นด้วย (")
+    public void case02() {
+        DemoRange demoRange = new DemoRange("(1,5]");
+        boolean result = demoRange.startWithInclude();
+        assertFalse(result);
     }
 
 }
