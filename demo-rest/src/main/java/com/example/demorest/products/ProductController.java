ed4c089afd653c357d7ce753f22609c572e9de80
up1 <somkiat.p@gmail.com>
2021-12-07 16:21:37 +0700

    Create first controller

---
+++ demo-rest/src/main/java/com/example/demorest/products/ProductController.java
@@ -0,0 +1,9 @@
+package com.example.demorest.products;
+
+public class ProductController {
+
+    public ProductResponse getProductById(int id) {
+        return new ProductResponse();
+    }
+
+}
---
+++ demo-rest/src/main/java/com/example/demorest/products/ProductResponse.java
@@ -0,0 +1,31 @@
+package com.example.demorest.products;
+
+public class ProductResponse{
+	private double price;
+	private String name;
+	private int id;
+
+	public void setPrice(double price){
+		this.price = price;
+	}
+
+	public double getPrice(){
+		return price;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public int getId(){
+		return id;
+	}
+}
