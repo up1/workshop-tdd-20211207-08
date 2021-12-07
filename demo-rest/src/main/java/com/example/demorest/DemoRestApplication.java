e222ed6d9feeb24a8f3ece71e5e3fd761dab0ac1
up1 <somkiat.p@gmail.com>
2021-12-07 16:00:20 +0700

    Number of beans

--- demo-rest/src/main/java/com/example/demorest/DemoRestApplication.java
+++ demo-rest/src/main/java/com/example/demorest/DemoRestApplication.java
@@ -2,12 +2,20 @@ package com.example.demorest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoRestApplication.class, args);
+		ConfigurableApplicationContext context
+				= SpringApplication.run(DemoRestApplication.class, args);
+		String[] beans = context.getBeanDefinitionNames();
+		for (String bean : beans) {
+			System.out.println(bean);
+		}
+
+		System.out.println(context.getBeanDefinitionCount());
 	}
 
 }
