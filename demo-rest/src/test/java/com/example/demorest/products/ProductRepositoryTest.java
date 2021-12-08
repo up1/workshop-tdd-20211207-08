6ee7868ecf55eb982d01ab1932702f1fbb079f2e
up1 <somkiat.p@gmail.com>
2021-12-08 10:52:17 +0700

    Repository testing

---
+++ demo-rest/src/test/java/com/example/demorest/products/ProductRepositoryTest.java
@@ -0,0 +1,21 @@
+package com.example.demorest.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class ProductRepositoryTest {
+
+    @Test
+    public void case_01() {
+
+    }
+
+    @Test
+    public void case_02() {
+
+    }
+
+}
