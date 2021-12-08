8833758d707e6faa633bfd101eaeb18181c68385
up1 <somkiat.p@gmail.com>
2021-12-08 14:02:57 +0700

    Service testing with mock repository

--- demo-rest/src/test/java/com/example/demorest/products/ProductServiceTest.java
+++ demo-rest/src/test/java/com/example/demorest/products/ProductServiceTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ProductServiceTest {
@@ -13,8 +17,15 @@ public class ProductServiceTest {
     @Autowired
     private ProductService productService;
 
+    @MockBean
+    private ProductRepository repository;
+
     @Test
     public void getById() {
+        // Arrange
+        Product product1 = new Product("Product 1", 100);
+        when(repository.findById(1))
+                .thenReturn(Optional.of(product1));
         // Act
         ProductResponse response = productService.getById(1);
         // Assert
