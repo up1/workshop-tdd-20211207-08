e98d36bc611c6c93348e6038d6ce9093454de0f0
up1 <somkiat.p@gmail.com>
2021-12-08 11:01:54 +0700

    Add get product by id

--- demo-rest/src/main/java/com/example/demorest/products/Product.java
+++ demo-rest/src/main/java/com/example/demorest/products/Product.java
@@ -14,6 +14,14 @@ public class Product {
     private String name;
     private double price;
 
+    public Product() {
+    }
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
     public int getId() {
         return id;
     }
--- demo-rest/src/test/java/com/example/demorest/products/ProductRepositoryTest.java
+++ demo-rest/src/test/java/com/example/demorest/products/ProductRepositoryTest.java
@@ -1,21 +1,30 @@
 package com.example.demorest.products;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 public class ProductRepositoryTest {
 
+    @Autowired
+    private ProductRepository repository;
+
     @Test
     public void case_01() {
-
+        // Arrange
+        Product product1 = new Product("demo 01", 100);
+        repository.save(product1);
+        // Act
+        Optional<Product> result = repository.findById(1);
+        assertTrue(result.isPresent());
+        assertEquals(1, result.get().getId());
+        assertEquals("demo 01", result.get().getName());
     }
 
-    @Test
-    public void case_02() {
-
-    }
 
 }
