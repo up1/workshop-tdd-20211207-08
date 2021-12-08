f2bad08dba5108e569e5e90e3c560a58c3a31308
up1 <somkiat.p@gmail.com>
2021-12-08 14:56:27 +0700

    Not found case

--- demo-rest/src/test/java/com/example/demorest/products/ProductServiceUnitTest.java
+++ demo-rest/src/test/java/com/example/demorest/products/ProductServiceUnitTest.java
@@ -33,4 +33,19 @@ public class ProductServiceUnitTest {
         assertEquals(1, response.getId());
         assertEquals("Product 1", response.getName());
     }
+
+    @Test
+    public void case_not_found() {
+        // Arrange
+        when(repository.findById(1))
+                .thenReturn(Optional.empty());
+
+        ProductService productService = new ProductService(otherService, repository);
+        // Act
+        Exception e = assertThrows(ProductNotFoundException.class,
+                ()-> {
+                    productService.getById(1);
+                });
+        assertEquals("Product not found with id=1", e.getMessage());
+    }
 }
