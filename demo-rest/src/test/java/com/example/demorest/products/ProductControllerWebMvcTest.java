e484d72c3175998a51d0b11bcb8516088952a52e
up1 <somkiat.p@gmail.com>
2021-12-08 10:16:01 +0700

    Use mock bean in WebMvcTest

--- demo-rest/src/test/java/com/example/demorest/products/ProductControllerWebMvcTest.java
+++ demo-rest/src/test/java/com/example/demorest/products/ProductControllerWebMvcTest.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -18,8 +20,15 @@ public class ProductControllerWebMvcTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private ProductService productService;
+
     @Test
     public void getProductById() throws Exception {
+        // Arrange
+        when(productService.getById(1))
+                .thenReturn(new ProductResponse( 1, "Stub name 1", 200.50 ));
+
         MvcResult response = mvc.perform(get("/products/1").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
@@ -28,6 +37,6 @@ public class ProductControllerWebMvcTest {
         ProductResponse result = objectMapper.readValue(responseByte, ProductResponse.class);
         // Assert
         assertEquals(1, result.getId());
-        assertEquals("Product 1", result.getName());
+        assertEquals("Stub name 1", result.getName());
     }
 }
