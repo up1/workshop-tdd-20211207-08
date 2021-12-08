9b39d9f5aa22d0d4bf982d69402229598440b2f6
up1 <somkiat.p@gmail.com>
2021-12-08 15:37:08 +0700

    Gateway testing

---
+++ demo-rest/src/test/java/com/example/demorest/users/UserGatewayTest.java
@@ -0,0 +1,15 @@
+package com.example.demorest.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class UserGatewayTest {
+
+    @Test
+    public void getById() {
+    }
+}
