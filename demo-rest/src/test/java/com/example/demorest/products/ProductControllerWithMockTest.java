1a9a23ed47fae33c36d4b3add08ff04ac90bb484
up1 <somkiat.p@gmail.com>
2021-12-08 12:13:04 +0700

    Mock product repositry

--- demo-rest/src/test/java/com/example/demorest/products/ProductControllerWithMockTest.java
+++ demo-rest/src/test/java/com/example/demorest/products/ProductControllerWithMockTest.java
@@ -7,7 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ProductControllerWithMockTest {
@@ -18,9 +21,18 @@ public class ProductControllerWithMockTest {
     @MockBean
     private OtherService otherService;
 
+    @MockBean
+    private ProductRepository repository;
+
     @Test
     public void getProductById() {
+        // Arrange
+        Product product1 = new Product("Product 1", 100);
+        when(repository.findById(1))
+                .thenReturn(Optional.of(product1));
+        // Act
         ProductResponse result = restTemplate.getForObject("/products/1", ProductResponse.class);
+        // Assert
         assertEquals(1, result.getId());
         assertEquals("Product 1", result.getName());
     }
