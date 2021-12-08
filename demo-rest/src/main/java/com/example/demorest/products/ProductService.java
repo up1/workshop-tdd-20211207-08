2fb0f56c0c0cffd90503bfee1ce180fa6ad91a79
up1 <somkiat.p@gmail.com>
2021-12-08 11:42:51 +0700

    Service call repository

--- demo-rest/src/main/java/com/example/demorest/products/ProductService.java
+++ demo-rest/src/main/java/com/example/demorest/products/ProductService.java
@@ -3,14 +3,23 @@ package com.example.demorest.products;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ProductService {
 
     @Autowired
     private OtherService otherService;
 
+    @Autowired
+    private ProductRepository repository;
+
     public ProductResponse getById(int id) {
         otherService.doSth();
-        return new ProductResponse( id, "Product 1", 200.50 );
+        // Get data from database
+        Optional<Product> result = repository.findById(id);
+        Product product = result.get();
+        // Map entity to response
+        return new ProductResponse( id, product.getName(), product.getPrice() );
     }
 }
